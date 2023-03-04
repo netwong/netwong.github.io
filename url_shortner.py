1a445ef97944598b8277e52d826be7e418f54145
cksir <netwong@gmail.com>
2023-03-04 22:01:40 +0800

    Update url_shortner.py

--- url_shortner.py
+++ url_shortner.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pyshortners as pyst
+
+shortners = pyst.Shortner()
+
 st.markdown("<h1 style='text-align: centre;'>URL Shortner</h1>", unsafe_allow_html=True)
 form=st.form("name")
 url=form.text_input("URL HERE")
-form.form_submit_button("SHORT")
+s_btn = form.form_submit_button("SHORT")
+if s_btn:
+  shorted_url = tinyurl.short(url)
+  st.text(shorted_url)
+  
 
