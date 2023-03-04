6d5bb4c1bab07c97e829001262fca6d20f5bf210
cksir <netwong@gmail.com>
2023-03-04 21:51:05 +0800

    Update url_shortner.py

--- url_shortner.py
+++ url_shortner.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.markdown("<h1 style='text-align: centre;'>URL Shortner</h1>", unsafe_allow_html=True)
 form=st.form("name")
 url=form.text_input("URL HERE")
+form.form_submit_button("SHORT")
+
