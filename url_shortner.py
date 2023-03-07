dabcc33ab0379553ea1ff1ab25211fbab62e6cd6
cksir <netwong@gmail.com>
2023-03-07 22:55:59 +0800

    Update url_shortner.py

--- url_shortner.py
+++ url_shortner.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import pyshorteners as pyst
 
+st.markdown("""
+.css-1rs6os.edgvbvh3
+{
+  visibility: hidden;
+}
+
+.css-cio0dv.egzxvld1
+{
+  visibility: hidden;
+}
+
+.css-z3au9t.egzxvld2
+{
+  visibility: hidden;
+}
+
+
+""", unsafe_allow_html=True;)
+
+
 shortners = pyst.Shortener()
 
 st.markdown("<h1 style='text-align: centre;'>URL Shortner</h1>", unsafe_allow_html=True)
