c2ed639ab99ec90bb0d25f0b2140236ddd41f913
cksir <netwong@gmail.com>
2023-03-08 10:08:01 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,3 +5,7 @@ st.header("Demo page")
 st.markdwon("---")
 
 st.title("Uploading Files")
+image = st.file_uploader("Please upload an image", type=["png", "jpg"])
+if image is not None:
+  st.image(image)
+  
