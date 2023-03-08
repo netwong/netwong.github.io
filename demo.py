423f7141a08260d8fe8201ba4fe5df1fbf49ca0d
cksir <netwong@gmail.com>
2023-03-08 10:19:53 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,6 +9,13 @@ image = st.file_uploader("Please upload an image", type=["png", "jpg"])
 if image is not None:
   st.image(image)
   
+
+images = st.file_uploader("Please upload images", type=["png", "jpg"], accept_multiple_files=True)
+if images is not None:
+  for image in images:
+    st.image(image)  
+  
+  
 video = st.file_uploader("Please upload a video", type="mp4")
 if video is not None:
   st.video(video)
