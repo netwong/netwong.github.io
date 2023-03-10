f19428c7abff4f0a063d23ab34ed00ae93f787cf
cksir <netwong@gmail.com>
2023-03-10 15:33:02 +0800

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.markdown("<h1 style='text-align:center'>User Registration</h1>", unsafe_allow_html=True)
 with st.form("Form 1"):
   col1, col2 = st.columns(2)
-  col1.text_input("First Name")
-  col2.text_input("Last Name")
+  fName = col1.text_input("First Name")
+  lName = col2.text_input("Last Name")
   st.text_input("Email Address")
   st.text_input("Password")
   st.text_input("Confirm Password")
@@ -13,5 +13,8 @@ with st.form("Form 1"):
   day.text_input("Day")
   month.text_input("Month")
   year.text_input("Year")
-  st.form_submit_button('Submit')
-  
+  fState = st.form_submit_button('Submit')
+  if fState:
+    if fName = "" and lName = "":
+      st.warning("Please fill above fields")
+      
