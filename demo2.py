f68013b03dd4c5c8d6cd8edcb860a3efcdef4de4
cksir <netwong@gmail.com>
2023-03-10 15:26:02 +0800

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -7,6 +7,7 @@ with st.form("Form 1"):
   col1.text_input("First Name")
   col2.text_input("Last Name")
   st.text_input("Email Address")
+  col1, col2 = st.columns(2)
   col1.text_input("Password")
   col2.text_input("Confirm Password")
   st.form_submit_button('Submit')
