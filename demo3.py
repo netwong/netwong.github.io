4b1a124adb927732c9f30b0b63e99d024e71ed97
cksir <netwong@gmail.com>
2023-03-11 12:20:12 +0800

    Update demo3.py

--- demo3.py
+++ demo3.py
@@ -7,18 +7,23 @@ import numpy as np
 
 
 x = np.linspace(0,10,100)
-bar_x=[1,2,3,4,5]
+bar_x=np.array([1,2,3,4,5])
 
 opt = st.sidebar.radio("Select any graph", options=("Line", "Bar", "H-Bar"))
 if opt == "Line":
-  st.markdown("<h1 style='text-align:center;'>Line Chart</h1>")
+  st.markdown("<h1 style='text-align:center;'>Line Chart</h1>", unsafe_allow_html=True)
   fig = plt.figure()
   # plt.style.use("https://github.com/dhaitz/matplotlib-stylesheets/blob/master/pacoty.mplstyle")
   plt.plot(x, np.sin(x))
   plt.plot(x, np.cos(x), '--')
   st.write(fig)
 elif opt == "Bar":
-  st.markdown("<h1 style='text-align:center;'>Line Chart</h1>")
+  st.markdown("<h1 style='text-align:center;'>Bar Chart</h1>", unsafe_allow_html=True)
   fig = plt.figure()
-  plt.bar(x, x*10)
+  plt.bar(bar_x, bar_x*10)
+  st.write(fig)
+else:
+  st.markdown("<h1 style='text-align:center;'>H-Bar Chart</h1>", unsafe_allow_html=True)
+  fig = plt.figure()  
+  plt.barh(bar_x*10, bar_x)
   st.write(fig)
