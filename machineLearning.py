65acef2e0570cf8ad22aa542c9a1951d33a499a3
cksir <netwong@gmail.com>
2023-03-11 21:29:11 +0800

    Update machineLearning.py

--- machineLearning.py
+++ machineLearning.py
@@ -33,7 +33,16 @@ def add_parameter_ui(clf_name):
   if clf_name == "KNN":
     K = st.sidebar.slider("K", 1, 15)
     params["K"] = K
+  elif clf_name == "SVM":
+    C = st.sidebar.slider("C", 0.01, 10.0)
+    params["C"] = C  
+  else:
+    max_depth = st.sidebar.slider("max_depth", 2, 15)
+    n_estimators = st.sidebar.slider("n_estimators", 1, 100)
+    params["max_depth"] = max_depth
+    params["n_estimators"] = n_estimators
   return params
+  
 
 add_parameter_ui(classifer_name)
 
