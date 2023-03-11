4894837606df6717bf6472db57946a7a57a71c2d
cksir <netwong@gmail.com>
2023-03-11 09:25:19 +0800

    Update demo3.py

--- demo3.py
+++ demo3.py
@@ -6,4 +6,9 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
+x = np.linspace(0,10,100)
 st.sidebar.write("Hello this is my side bar")
+
+fig = plt.figure()
+plt.plot(x, np.sin(x))
+st.write(fig)
