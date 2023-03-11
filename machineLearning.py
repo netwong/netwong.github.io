283eaebee2d55e44d7999d575f7414b18f50508b
cksir <netwong@gmail.com>
2023-03-11 18:53:39 +0800

    Update machineLearning.py

--- machineLearning.py
+++ machineLearning.py
@@ -8,6 +8,24 @@ st.write("""
 which one is the best?
 """)
 
-dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Brest Cancer", "Wine Dataset"))
+dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Brest Cancer", "Wine"))
 classifer_name = st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Random Forest"))
+
+def get_dataset(dataset_name):
+  if dataset_name == "Iris":
+    data = dataset.load_iris()
+  elsif dataset_name == "Breast Cancer":
+    data = dataset.load_brest_cancer()
+  else:
+    data = dataset.load_wine()
+    
+  X = data.data
+  y = data.target
+  return X, y
+
+
+X, y = get_dataset(dataset_name)
+st.write("shape of dataset", X.shape)
+st.write("number of classes", len(np.unique(y)))
+
 
