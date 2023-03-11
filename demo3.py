78691f4a4de0cd03c3ac6e548c59d9a5ac08da65
cksir <netwong@gmail.com>
2023-03-11 09:48:43 +0800

    Update demo3.py

--- demo3.py
+++ demo3.py
@@ -8,7 +8,7 @@ import numpy as np
 
 x = np.linspace(0,10,100)
 opt = st.sidebar.radio("Select any graph", options=("Line", "Bar", "H-Bar"))
-if opt == "Line"
+if opt == "Line":
   fig = plt.figure()
   plt.style.use("https://github.com/dhaitz/matplotlib-stylesheets/blob/master/pitayasmoothie-dark.mplstyle")
   plt.plot(x, np.sin(x))
